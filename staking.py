bf9af7201eb602bb0be5ddbfd1965b6487cc36a7
Vishant02 <vishant.mittal233@gmail.com>
2022-03-10 11:27:04 +0530

    basic contract structure

--- staking.py
+++ staking.py
@@ -1,2 +1,42 @@
 from pyteal import *
 #staking contract
+def approval_program():
+    program = Cond(
+        [Txn.application_id() == Int(0), on_creation],
+        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
+        [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
+        [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
+        [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
+        [Txn.on_completion() == OnComplete.NoOp, handle_noop],
+    )
+
+    handle_noop = Seq([
+        Approve()
+    ])
+
+    on_creation = Seq([
+        Approve()
+    ])
+
+    handle_optin = Seq([
+        Approve()
+    ])
+
+    handle_closeout = Seq([
+        Approve()
+    ])
+
+    handle_updateapp = Seq([
+        Approve()
+    ])
+
+    handle_deleteapp = Seq([
+        Approve()
+    ])
+
+    return compileTeal(program, Mode.Application, version = 5)
+
+
+def clear_state_program():
+    program = Return(Int(1))
+    return compileTeal(program, Mode.Application, version=5)
